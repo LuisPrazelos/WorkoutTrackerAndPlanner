0e58f203c19b1529b753dd776c73f1b6a6fc9857
luiskeplays <luisprazelos@gmail.com>
2026-04-08 16:25:16 +0200

    this aint working bih

--- api/index.php
+++ api/index.php
@@ -4,20 +4,4 @@
 error_reporting(E_ALL & ~E_DEPRECATED);
 
 // Forward Vercel requests to the normal Laravel index file
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (Throwable $e) {
-    error_log('VERCEL_BOOT_ERROR: ' . $e::class . ': ' . $e->getMessage());
-    error_log('VERCEL_BOOT_ERROR_FILE: ' . $e->getFile() . ':' . $e->getLine());
-    error_log($e->getTraceAsString());
-
-    http_response_code(500);
-
-    if ((bool) (getenv('APP_DEBUG') ?: false)) {
-        header('Content-Type: text/plain; charset=UTF-8');
-        echo $e::class . ': ' . $e->getMessage() . PHP_EOL;
-        echo $e->getFile() . ':' . $e->getLine() . PHP_EOL;
-    } else {
-        echo 'Internal Server Error';
-    }
-}
+require __DIR__ . '/../public/index.php';
