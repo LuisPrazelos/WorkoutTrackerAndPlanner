2621fbc66283b44f684f33ca5d6a1a6e2eb63b82
luiskeplays <luisprazelos@gmail.com>
2026-04-08 15:40:02 +0200

    fix: silence php 8.5 deprecation warnings

--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// Silence PHP 8.5 deprecation warnings from vendor files
+error_reporting(E_ALL & ~E_DEPRECATED);
+
 // Forward Vercel requests to the normal Laravel index file
 // Deployment update trigger
 require __DIR__ . '/../public/index.php';
